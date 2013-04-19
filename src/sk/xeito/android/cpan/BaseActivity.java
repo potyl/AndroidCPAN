62254c16ce024aac3311719411085660035414d8
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2013-04-19 23:38:45 +0200

    Add a TODO for the preferences

--- src/sk/xeito/android/cpan/BaseActivity.java
+++ src/sk/xeito/android/cpan/BaseActivity.java
@@ -3,6 +3,7 @@ package sk.xeito.android.cpan;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class BaseActivity extends Activity {
 
@@ -12,8 +13,20 @@ public class BaseActivity extends Activity {
 	}
 
 	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				Utils.printf("Invoking preferences");
+				// TODO put preferences here
+			return true;
+
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
